4a8731f6d53645867ecc69106215960d37efa240
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-22 02:17:25 +0300

    Ruff's issues

--- cstvis/collector.py
+++ cstvis/collector.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
-from typing import Callable, List, Optional, Union
 from functools import partial
+from typing import Callable, List, Optional, Union
 
 from libcst import CSTNode
 
